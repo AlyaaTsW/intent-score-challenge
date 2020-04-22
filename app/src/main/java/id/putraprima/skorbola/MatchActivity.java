2f59f03b9616ab095c58f0d4421de99ae59bc518
AlyaaTsW <60677174+AlyaaTsW@users.noreply.github.com>
2020-04-22 12:35:39 +0700

    asdfghj

--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -1,19 +1,101 @@
 package id.putraprima.skorbola;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MatchActivity extends AppCompatActivity {
+
+    private TextView text_home;
+    private TextView text_away;
+    private ImageView logo_home;
+    private ImageView logo_away;
+    private Integer home_skor;
+    private Integer away_skor;
+    private TextView skor_home;
+    private TextView skor_away;
+
+    private TextView sc_home;
+
+    private static final String RESULT_KEY = "result";
+    private static final String TAG = MatchActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+        text_home = findViewById(R.id.txt_home);
+        text_away = findViewById(R.id.txt_away);
+        logo_home = findViewById(R.id.home_logo);
+        logo_away = findViewById(R.id.away_logo);
+        skor_home = findViewById(R.id.score_home);
+        skor_away = findViewById(R.id.score_away);
+        sc_home = findViewById(R.id.sc_home);
+
+        Bundle extra = getIntent().getExtras();
+        if (extra != null){
+            //1.Menampilkan detail match sesuai data dari main activity
+            Bundle bundle = getIntent().getExtras();
+            Bitmap home = bundle.getParcelable("home_logo");
+            Bitmap away = bundle.getParcelable("away_logo");
+
+            logo_home.setImageBitmap(home);
+            logo_away.setImageBitmap(away);
+
+            text_home.setText(extra.getString("home_team"));
+            text_away.setText(extra.getString("away_team"));
+        }
         //TODO
         //1.Menampilkan detail match sesuai data dari main activity
-        //2.Tombol add score menambahkan memindah activity ke scorerActivity dimana pada scorer activity di isikan nama pencetak gol
-        //3.Dari activity scorer akan mengirim kembali ke activity matchactivity otomatis nama pencetak gol dan skor bertambah +1
-        //4.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang beserta nama pencetak gol ke ResultActivity, jika seri di kirim text "Draw",
+        //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
+        //3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==1){
+            home_skor++;
+            skor_home.setText(String.valueOf(home_skor));
+            String nama = data.getStringExtra("nama");
+            sc_home.setText(nama);
+        }
+    }
+
+    public void handleAddHome(View view) {
+        Intent intent = new Intent(MatchActivity.this, ScorerActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    public void handleAddAway(View view) {
+        away_skor++;
+        skor_away.setText(String.valueOf(away_skor));
+    }
+
+    public void handleResult(View view) {
+        String result;
+
+        if (home_skor > away_skor){
+            result = text_home.getText().toString();
+        } else if (home_skor < away_skor){
+            result = text_away.getText().toString();
+        } else {
+            result = "DRAW!";
+        }
+
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra(RESULT_KEY, result);
+        startActivity(intent);
     }
 }
--- app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
@@ -2,13 +2,50 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ScorerActivity extends AppCompatActivity {
+
+    private Integer home_skor;
+    private Integer away_skor;
+    private EditText sc_name1;
+    private Button button;
+
+    String[] hname = new String[30];
+    String[] aname = new String[30];
+
+    private String key;
+
+    private static final String SCORER_HOME = "scorer_home";
+    private static final String SCORER_AWAY = "scorer_away";
+    private static final String SKOR_HOME = "skor_home";
+    private static final String SKOR_AWAY = "skor_away";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scorer);
+        sc_name1 = findViewById(R.id.sc_name);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String name = sc_name1.getText().toString();
+                Bundle bundle = new Bundle();
+                if (!(name).equals("")){
+                    Intent intent = new Intent();
+                    intent.putExtra("nama", name);
+                    setResult(1, intent);
+                    finish();
+                }
+            }
+        });
     }
 }
